5052f9424a832da209d8eb83c6d035c8570f1a00
Nalin <nalindalal2004@gmail.com>
2025-07-24 12:34:50 +0530

    updated previous file and code to remove merge conflicts

--- packages/better-auth/src/plugins/organization/has-permission.ts
+++ packages/better-auth/src/plugins/organization/has-permission.ts
@@ -2,44 +2,44 @@ import type { OrganizationOptions } from "./types";
 import { defaultRoles } from "./access";
 
 type PermissionExclusive =
-	| {
-			/**
-			 * @deprecated Use `permissions` instead
-			 */
-			permission: { [key: string]: string[] };
-			permissions?: never;
-	  }
-	| {
-			permissions: { [key: string]: string[] };
-			permission?: never;
-	  };
+  | {
+      /**
+       * @deprecated Use `permissions` instead
+       */
+      permission: { [key: string]: string[] };
+      permissions?: never;
+    }
+  | {
+      permissions: { [key: string]: string[] };
+      permission?: never;
+    };
 
 export const hasPermission = (
-	input: {
-		role: string;
-		options: OrganizationOptions;
-		allowCreatorAllPermissions?: boolean;
-	} & PermissionExclusive,
+  input: {
+    role: string;
+    options: OrganizationOptions;
+    allowCreatorAllPermissions?: boolean;
+  } & PermissionExclusive,
 ) => {
-	if (!input.permissions && !input.permission) {
-		return false;
-	}
+  if (!input.permissions && !input.permission) {
+    return false;
+  }
 
-	const roles = input.role.split(",");
-	const acRoles = input.options.roles || defaultRoles;
+  const roles = input.role.split(",");
+  const acRoles = input.options.roles || defaultRoles;
 
-	const creatorRole = input.options.creatorRole || "owner";
-	const isCreator = roles.includes(creatorRole);
+  const creatorRole = input.options.creatorRole || "owner";
+  const isCreator = roles.includes(creatorRole);
 
-	const allowCreatorsAllPermissions = input.allowCreatorAllPermissions || false;
-	if (isCreator && allowCreatorsAllPermissions) return true;
+  const allowCreatorsAllPermissions = input.allowCreatorAllPermissions || false;
+  if (isCreator && allowCreatorsAllPermissions) return true;
 
-	for (const role of roles) {
-		const _role = acRoles[role as keyof typeof acRoles];
-		const result = _role?.authorize(input.permissions ?? input.permission);
-		if (result?.success) {
-			return true;
-		}
-	}
-	return false;
+  for (const role of roles) {
+    const _role = acRoles[role as keyof typeof acRoles];
+    const result = _role?.authorize(input.permissions ?? input.permission);
+    if (result?.success) {
+      return true;
+    }
+  }
+  return false;
 };
